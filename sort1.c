630e77bb0dbf2fe2bd51873bf7799f1fcd4ae847
Drozd433Mary <masha.drozdova.54@mail.ru>
2025-03-03 16:52:23 +0700

    addnew

---
+++ sort1.c
@@ -0,0 +1,25 @@
+// сортировка CountingSort 
+void countingSort(struct Student students[], int size) {
+    int max_total = 0;
+    for (int i = 0; i < size; i++) {
+        if (students[i].obb > max_total) {
+            max_total = students[i].obb;
+        }
+    }
+    int count[max_total + 1];
+    memset(count, 0, sizeof(count));
+    for (int i = 0; i < size; i++) {
+        count[students[i].obb]++;
+    }
+    for (int i = max_total - 1; i >= 0; i--) {
+        count[i] += count[i + 1];
+    }
+    struct Student output[size];
+    for (int i = size - 1; i >= 0; i--) {
+        output[count[students[i].obb] - 1] = students[i];
+        count[students[i].obb]--;
+    }
+    for (int i = 0; i < size; i++) {
+        students[i] = output[i];
+    }
+}
